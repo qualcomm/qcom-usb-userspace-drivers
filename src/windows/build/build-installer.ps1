f8e25bb0e012275286bcff3853111b026843fc54
Chenxi Han <chehan@qti.qualcomm.com>
2026-06-14 22:19:38 -0700

    script: add license header and copyright info
    Signed-off-by: Chenxi Han <chehan@qti.qualcomm.com>

--- src/windows/build/build-installer.ps1
+++ src/windows/build/build-installer.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
 param(
     [string]$OutputName = "installer.exe",
     [ValidateSet("x64", "x86", "arm64")]
--- src/windows/build/build_installer.bat
+++ src/windows/build/build_installer.bat
@@ -1,9 +1,12 @@
+:: Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+:: SPDX-License-Identifier: BSD-3-Clause
+
 @echo off
 setlocal enabledelayedexpansion
 
 set "SCRIPT_DIR=%~dp0"
 
-for %%A in (x86 x64 arm64) do (
+for %%A in (x86 x64) do (
     REM Build tools for this architecture
     powershell -ExecutionPolicy Bypass -File "%SCRIPT_DIR%build_tools.ps1" -Platform %%A
     if !ERRORLEVEL! neq 0 exit /b !ERRORLEVEL!
